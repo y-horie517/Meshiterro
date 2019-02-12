4196e2e5dc51585df7503f8c5a278aa7663b857b
yhorie517 <yhkovoodoo@gmail.com>
2019-02-12 15:30:08 +0900

    [Finish]5-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameteres, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameteres
+		# ユーザ登録（sign_up）の時に、ユーザ名(name)のデータ操作を許可する。
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	end
 end
