b16b56f8b63a76c6c181d3594d6366e82b17571f
yhorie517 <yhkovoodoo@gmail.com>
2019-02-13 18:06:21 +0900

    [Finish]5-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -23,7 +23,7 @@ class PostImagesController < ApplicationController
 		@post_comment = PostComment.new
 	end
 
-	def method_name
+	def destroy
 		@post_image = PostImage.find(params[:id])
 		@post_image.destroy
 		redirect_to post_images_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  # get 'users/show' 下と重なるため削除
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
