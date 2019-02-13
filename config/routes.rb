ed4bc02fb58bfb210104f2c3cbcdc4ef72ed75b0
yhorie517 <yhkovoodoo@gmail.com>
2019-02-14 00:54:23 +0900

    [Finish]5-6

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   	resource :post_comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 end
